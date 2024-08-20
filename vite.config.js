e4e1c5de79f9a9d33de5bdb4f1329dccf9182f28
Palm Civet <palmcivet_email@qq.com>
2024-08-20 22:56:29 +0800

    chore: update config

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,21 @@ import pkg from "./package.json";
 
 const BANNER = `/* eslint-disable */
 // ==UserScript==
-// @name        ${pkg.name}
-// @name:zh-CN  ${pkg.name}
-// @namespace   ${pkg.repository}
-// @version     ${pkg.version}
-// @license     ${pkg.license}
-// @author      ${pkg.author}
-// @updateURL   https://palmcivet.github.io/${pkg.repository}/main.js
-// @description ${pkg.name} is a versatile script designed to enhance your Figma experience by allowing you to reposition the toolbelt with ease.
-// @match       https://www.figma.com/design/*
-// @grant       GM_setValue
-// @grant       GM_getValue
-// @grant       GM_addStyle
-// @grant       GM_registerMenuCommand
-// @grant       GM_unregisterMenuCommand
+// @name              ${pkg.name}
+// @name:zh-CN        ${pkg.name}
+// @namespace         ${pkg.repository}
+// @version           ${pkg.version}
+// @license           ${pkg.license}
+// @author            ${pkg.author}
+// @updateURL         https://palmcivet.github.io/${pkg.repository}/main.js
+// @description       ${pkg.name} is a versatile script designed to enhance your Figma experience by allowing you to reposition the toolbelt with ease.
+// @description:zh-CN ${pkg.name} 是一个用于增强 Figma 体验的脚本，可以轻松地将工具栏调整到想要的位置。
+// @match             https://www.figma.com/design/*
+// @grant             GM_setValue
+// @grant             GM_getValue
+// @grant             GM_addStyle
+// @grant             GM_registerMenuCommand
+// @grant             GM_unregisterMenuCommand
 // ==/UserScript==`;
 
 export default defineConfig({
